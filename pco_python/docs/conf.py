4ab1c8d9a9e6592cd700b7908c5befa71abe9de1
mwlon <m.w.loncaric@gmail.com>
2025-04-20 20:07:28 +0800

    fix ruff lints

--- pco_python/docs/conf.py
+++ pco_python/docs/conf.py
@@ -1,3 +1,7 @@
+import toml
+# I don't know why, but sphinx fails to import pcodec during autodoc without this
+import pcodec # noqa: F401
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -9,10 +13,6 @@
 # taking a lot of ideas from
 # https://github.com/delta-io/delta-rs/blob/main/python/docs/source/conf.py
 
-import os
-import sys
-import toml
-
 project = 'Pcodec'
 copyright = '2025, Pcodec devs'
 author = 'Pcodec devs'
@@ -22,8 +22,7 @@ def get_release_version() -> str:
 
 version = get_release_version()
 
-import pcodec # I don't know why, but sphinx fails to import pcodec during autodoc without this
-from pcodec import standalone, wrapped
+# from pcodec import standalone, wrapped
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
